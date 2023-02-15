d7babe6b9ca478b639f635bd57cec6ee28262a99
Helio Albano de Oliveira <helio.albano@mercadolivre.com>
2023-02-14 23:11:25 -0300

    Implement users repository

--- api/src/models/user.go
+++ api/src/models/user.go
@@ -2,7 +2,7 @@ package models
 
 import "time"
 
-type Usuario struct {
+type User struct {
 	ID        uint64    `json:"id,omitempty"`
 	Name      string    `json:"name,omitempty"`
 	Nick      string    `json:"nick,omitempty"`
